6e780a1cbe135183bc7ef72d7f76abbe3b93b0f7
Алексей <@DESKTOP-D98N22D>
2022-02-28 16:32:09 +0300

    доработка ускорения падения

--- game.js
+++ game.js
@@ -6,12 +6,12 @@ var bg = new Image();
 var fg = new Image();
 var pipeUp = new Image();
 var pipeBottom = new Image();
-
 bird.src = "img/bird.png";
 bg.src = "img/bg.png"; 
 fg.src = "img/fg.png"; 
 pipeUp.src = "img/pipeUp.png"; 
 pipeBottom.src = "img/pipeBottom.png"; 
+var score = 0;
 
 var gap = 90;
 
@@ -19,11 +19,12 @@ var xPos = 10;
 var yPos = 150;
 
 var grav = 1.5;
-
 document.addEventListener("keydown",moveUp)
 
+var t1 = (new Date).getTime();
 function moveUp(){
-    yPos -= 30;
+    t1 = (new Date).getTime();
+    yPos -= 20;
 }
 var pipe = [];
 
@@ -31,14 +32,12 @@ pipe[0]={
     x: cvs.width,
     y: 0
 }
-
 function draw(){
     ctx.drawImage(bg,0,0);
     for (var i = 0; i < pipe.length; i++){
 
         ctx.drawImage(pipeUp, pipe[i].x,pipe[i].y);
         ctx.drawImage(pipeBottom, pipe[i].x,pipe[i].y + pipeUp.height + gap);
-
         pipe[i].x-=1;
 
         if (pipe[i].x == 125){
@@ -52,9 +51,16 @@ function draw(){
         }
         if (xPos + bird.width >= pipe[i].x 
             && xPos <= pipe[i].x + pipeUp.width 
-            && (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + pipeUp.height+gap)){
+            && (yPos <= pipe[i].y + pipeUp.height 
+                || yPos + bird.height >= pipe[i].y + pipeUp.height+gap) 
+                || yPos + bird.height >= cvs.height - fg.height){
             location.reload();
         }
+
+        if (pipe[i].x == 5){
+            score++;
+        }
+
     }
 
 
@@ -62,11 +68,24 @@ function draw(){
     ctx.drawImage(fg,0,cvs.height - fg.height);
     ctx.drawImage(bird,xPos,yPos);
 
-    yPos += grav;
+    var t2 = ((new Date).getTime() - t1)/100;
+
+    console.log(t2);
+
+    if ( t2 > 1 ){
+        yPos += t2;
+    }
+
+
+    ctx.fillStyle = "#000";
+    ctx.font = "20px Verdana";
+
+    ctx.fillText("Счет: " + score, 10, cvs.height - 20);
     requestAnimationFrame(draw);
 };
 pipeBottom.onload = draw();
 
 
 
-
+function fun(){
+}
