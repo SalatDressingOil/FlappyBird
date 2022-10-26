a1766e3e167c1bb9c5a09bec0f7580001619601d
Алексей <@DESKTOP-D98N22D>
2022-10-27 00:37:16 +0300

    вроде рабочая мимикрия под экраны

--- game.js
+++ game.js
@@ -13,7 +13,19 @@ vkBridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
 
 
 var cvs = document.getElementById("canvas");
+var zn = cvs.height/cvs.width;
+var zn1 = innerWidth/innerHeight
+
+cvs.height*=zn
+cvs.width*=zn
+
 var ctx = cvs.getContext("2d");
+ctx.fillStyle = "#000";
+ctx.font = 30*zn+"px Verdana";
+
+console.log(zn,zn1)
+console.log(innerWidth,innerHeight)
+console.log(cvs.width,cvs.height)
 
 var bird = new Image();
 var bg = new Image();
@@ -26,14 +38,41 @@ bg.src = "img/bg.png";
 fg.src = "img/fg.png"; 
 pipeUp.src = "img/pipeUp.png"; 
 pipeBottom.src = "img/pipeBottom.png";
+var aa = [bird,bg,fg,pipeUp,pipeBottom]
+for (var i=0;i<aa.length;i++){
+    aa[i].width*=zn
+    aa[i].height*=zn
+}
+
+/*
+var zn = bg.width/bg.height
+bg.height=innerHeight*1
+bg.width=bg.height*zn
+
+var zn = bird.width/bird.height
+bird.height=innerHeight*0.05
+bird.width=bird.height*zn
+
+var zn = fg.width/fg.height
+fg.height=innerHeight*0.05
+fg.width=fg.height*zn
+
+var zn = pipeUp.width/pipeUp.height
+pipeUp.height==innerHeight*0.05
+pipeUp.width=pipeUp.height*zn
+
+var zn = pipeBottom.width/pipeBottom.height
+pipeBottom.height=innerHeight*0.05
+pipeBottom.width=pipeBottom.height*zn
+*/
 
 var score = 0;
 
-var gap = 186;
+var gap = 186*zn;
 
 var xPos = 20;
 var yPos = 150;
-var grav = 1;
+var grav = 1*zn;
 
 document.addEventListener("keydown",moveUp);
 document.addEventListener("click",moveUp);
@@ -45,16 +84,16 @@ var k = 0;
 
 var z = 3;
 var non = 10
-var schet = 1;
-var pixel_pipe_move = 3
+var schet = 1*zn;
+var pixel_pipe_move = 3*zn
 
 function moveUp(){
     console.log('click')
     //alert('click')
     t1 = (new Date).getTime();
-    schet = 15;
-    non=1
-    z=3
+    schet = 15*zn;
+    non=1*zn
+    z=3*zn
     //yPos -= 20;
 }
 var pipe = [];
@@ -71,8 +110,18 @@ pipe.push({
     x: cvs.width,
     y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
 });
+//ctx.scale(2.5,2.5)
 function draw(){
-    ctx.drawImage(bg,0,0);
+    //ctx.save();
+    //ctx.clearCanvas();
+    //ctx.scale(bg.width / cvs.width, bg.height / cvs.height);
+    //bg.width=cvs.width
+    //bg.height=cvs.height
+    //ctx.save();
+    //ctx.clearCanvas();
+    //ctx.scale(1,1);
+    //ctx.scale(innerWidth / cvs.width, innerHeight / cvs.height);
+    ctx.drawImage(bg,0,0,bg.width,bg.height);
     for (var i = 0; i < pipe.length; i++){
         if (xPos + bird.width-5 >= pipe[i].x 
             && xPos <= pipe[i].x + pipeUp.width 
@@ -83,9 +132,9 @@ function draw(){
                     xPos = 20;
                     yPos = 150;
 
-                    schet = 1;
-                    non=10 
-                    z=3
+                    schet = 1*zn;
+                    non=10*zn
+                    z=3*zn
 
                     pipe = [];
                     
@@ -104,11 +153,11 @@ function draw(){
             pipe.shift()
         }
         console.log(pipe[i].x,pixel_pipe_move,pipe[i].x/pixel_pipe_move,pipe.length,pipe[i].x,pipe[i].y)
-        ctx.drawImage(pipeUp, pipe[i].x,pipe[i].y);
-        ctx.drawImage(pipeBottom, pipe[i].x,pipe[i].y + pipeUp.height + gap - pipe[i].gap_ran);
+        ctx.drawImage(pipeUp, pipe[i].x,pipe[i].y,pipeUp.width,pipeUp.height);
+        ctx.drawImage(pipeBottom, pipe[i].x,pipe[i].y + pipeUp.height + gap - pipe[i].gap_ran,pipeBottom.width,pipeBottom.height);
         pipe[i].x-=pixel_pipe_move;
 
-        if (pipe[i].x == 80){
+        if (Math.round(pipe[i].x/pixel_pipe_move) == 27){
             pipe.push({
                 gap_ran: Math.random()*10,
                 x: cvs.width,
@@ -148,7 +197,7 @@ function draw(){
         if ( t2 > 2 ){
             yPos += t2*grav;
         }
-    ctx.drawImage(fg,0,cvs.height - fg.height);
+    ctx.drawImage(fg,0,cvs.height - fg.height,fg.width,fg.height);
     //xPos=0
     //yPos=0
 
@@ -160,11 +209,10 @@ function draw(){
 
     ctx.setTransform(1,0,0,1,0,0);
     //ctx.fillRect(xPos+bird.width/2,yPos+bird.height/2,5,5)
-
-    ctx.fillStyle = "#000";
-    ctx.font = "30px Verdana";
     //ctx.strokeText("Счет: " + score, 10, cvs.height - 20);
     ctx.fillText("Счет: " + score, 10, cvs.height - 20, 1000);
+
+    //ctx.restore();
     requestAnimationFrame(draw);
 };
 pipeBottom.onload = draw();
